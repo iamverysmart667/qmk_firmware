709d0167392fea9b396bd5b6fe85ed2b61b1f092
azamat <seetwopluses@gmail.com>
2022-10-01 18:14:06 +0600

    First commit

---
+++ keyboards/crkbd/keymaps/azamat/rules.mk
@@ -0,0 +1,10 @@
+MOUSEKEY_ENABLE    = no    # Mouse keys
+RGBLIGHT_ENABLE    = no    # Enable WS2812 RGB underlight.
+OLED_ENABLE        = yes
+OLED_DRIVER        = SSD1306
+LTO_ENABLE         = yes
+CAPS_WORD_ENABLE   = yes
+BOOTMAGIC_ENABLE   = yes
+VIA_ENABLE         = yes
+COMMAND_ENABLE     = no
+EXTRAFLAGS 			  += -flto
